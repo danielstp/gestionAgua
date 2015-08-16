a7c46fba4dd8178981c8dc8424d0e521fe44f010
Daniel Saguez Tezanos Pinto <daniel@saguez.org>
2015-08-15 22:46:43 -0400

    Corregido el admin

--- aguaEmbotellada/clientes/admin.py
+++ aguaEmbotellada/clientes/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Cliente, Cobro, Venta
 
+@admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    fields = ["nombre","apellidos"]
-admin.site.register(Cliente)
-admin.site.register(Cobro)
-admin.site.register(Venta)
+    list_display = ('nombre','apellidos')
+
+
+@admin.register(Cobro)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ()#'Cliente.nombre','Cliente.apellidos')
+
+
+@admin.register(Venta)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ()#'Cliente.nombre','Cliente.apellidos')
+
